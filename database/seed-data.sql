deb765af0a6eee708d680b8f22101606c66966cf
gasperpodergajs <gasper.podergajs@inova.si>
2025-08-04 10:15:05 +0200

    moved database files, added seed sql

---
+++ database/seed-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO girigiri.users (username, email, display_name) VALUES
+('new_user_1', 'new1@example.com', 'some display name 1'),
+('new_user_2', 'new2@example.com', 'some display name 2'),
+('another_user', 'another@example.com', 'some display name 3')
