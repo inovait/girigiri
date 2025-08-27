d8ede56cead3098ae62d9e276da3abcdca793f5b
gasperpodergajs <gasper.podergajs@inova.si>
2025-08-27 20:13:22 +0200

    moved constants file

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-
-export const MIGRATION_HISTORY_TABLE = 'migration_history';
-export const TEMP_PREFIX = 'tmp_';
---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const MIGRATION_HISTORY_TABLE = 'migration_history'
+export const TEMP_PREFIX = 'tmp'
