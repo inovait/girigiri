7c2d7ab87710f1370132aa859f2723c71b26979b
gasperpodergajs <gasper.podergajs@inova.si>
2025-08-29 13:23:23 +0200

    added additional constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,6 @@
 export const MIGRATION_HISTORY_TABLE = 'migration_history'
-export const TEMP_PREFIX = 'tmp'
+export const TEMP_PREFIX = 'tmp__'
+export const CONTAINER_SERVICE = 'mysqldb' // see docker compose.yml as a reference
+export const DOCKER_UP_COMMAND = 'docker compose --env-file .env up -d --build mysqldb';
+export const DOCKER_DOWN_COMMAND = 'docker compose --env-file .env rm -sfv mysqldb';
+export const MAIN_APP_COMMAND = 'tsx src/main.ts check:migrations';
